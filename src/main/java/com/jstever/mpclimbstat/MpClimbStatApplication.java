274cee47092da062c446fb235fed39cbc1b617ea
Jared Stever <jaredstever@comcast.net>
2021-04-01 21:06:47 -0700

    Initial commit

---
+++ src/main/java/com/jstever/mpclimbstat/MpClimbStatApplication.java
@@ -0,0 +1,13 @@
+package com.jstever.mpclimbstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpClimbStatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MpClimbStatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jstever/mpclimbstat/MpClimbStatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstever.mpclimbstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpClimbStatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
