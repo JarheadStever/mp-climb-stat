c2ab889fd49376519893ae76777a399a6411f3dd
Jared Stever <jaredstever@comcast.net>
2021-04-14 14:51:58 -0400

    fix regenerator-runtime error with recharts

--- src-js/app.jsx
+++ src-js/app.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom'
 import TicksPage from "./components/TicksPage";
+import "regenerator-runtime/runtime";
 
 
 ReactDOM.render(<TicksPage/>, document.getElementById('content'));
